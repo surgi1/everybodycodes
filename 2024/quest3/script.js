a1ef4101d90180c266fbe0e1114265ae0c72af6e
surgi <surgi1@gmail.com>
2024-11-11 12:57:35 +0100

    initial commit

--- 2024/quest3/script.js
+++ 2024/quest3/script.js
@@ -3,7 +3,7 @@ let map = getMap(input);
 
 let getVal = map => map.flat().reduce((a, v) => a+v, 0)
 
-let advanceMap12 = map => {
+const advanceMap12 = map => {
     let res = map.map(row => row.slice());
     for (let y = 0; y < map.length; y++) for (let x = 0; x < map[y].length; x++) {
         if (map[y][x] == 0) res[y][x] = 0;
@@ -12,37 +12,33 @@ let advanceMap12 = map => {
     return res;
 }
 
-
-let advanceMap = map => {
+const advanceMap = map => {
     let res = map.map(row => row.slice());
     for (let y = 0; y < map.length; y++) for (let x = 0; x < map[y].length; x++) {
-        if (map[y][x] == 0) res[y][x] = 0;
-        else {
-            let arr = [];
-            for (let i = -1; i <= 1; i++) for (let j = -1; j <= 1; j++) {
-                if (j == 0 && i == 0) continue;
-                if (y+i < 0 || y+i > map.length-1 || x+j < 0 || x+j > map[0].length-1) {arr.push(0); continue;}
-                arr.push(map[y+i][x+j]);
-            }
-            res[y][x] = map[y][x] + ( arr.every(v => Math.abs(v - (map[y][x]+1)) <= 1) ? 1 : 0 ); 
+        if (map[y][x] == 0) {
+            res[y][x] = 0;
+            continue;
         }
+
+        let arr = [];
+        for (let i = -1; i <= 1; i++) for (let j = -1; j <= 1; j++) {
+            if (j == 0 && i == 0) continue;
+            arr.push(
+                y+i < 0 || y+i > map.length-1 || x+j < 0 || x+j > map[0].length-1 ? 0 : map[y+i][x+j]
+            );
+        }
+
+        res[y][x] = map[y][x] + ( arr.every(v => Math.abs(v - (map[y][x]+1)) <= 1) ? 1 : 0 ); 
     }
     return res;
 }
 
+let res = 0;
 
-
-let res = getVal(map);
-map = advanceMap(map);
-console.log(map, res);
-let iters = 0;
 while (getVal(map) > res) {
     res = getVal(map);
     map = advanceMap(map);
-    console.log(map, res);
-    iters++;
-    //if (iters > 300) break;
+    //console.log(map, res);
 }
-console.log(getVal(map));
 
-// p3: 11338 len wrong, first char wrong
+console.log('p3', getVal(map));
--- 2024/quest4/script.js
+++ 2024/quest4/script.js
@@ -1,19 +1,13 @@
 let arr = input.split("\n").map(Number);
 
-let min = Math.min(...arr);
-
-function median(numbers) {
+const median = numbers => {
     const sorted = Array.from(numbers).sort((a, b) => a - b);
     const middle = Math.floor(sorted.length / 2);
-
-    if (sorted.length % 2 === 0) {
-        return (sorted[middle - 1] + sorted[middle]) / 2;
-    }
-
-    return sorted[middle];
+    return (sorted.length % 2 === 0) ? (sorted[middle - 1] + sorted[middle]) / 2 : sorted[middle];
 }
 
-//console.log(arr.reduce((a, v) => a + v-min, 0) )
+//let min = Math.min(...arr);
+//console.log('p2', arr.reduce((a, v) => a + v-min, 0) );
 
 let med = median(arr);
-console.log(arr.reduce((a, v) => a + Math.abs(v-med), 0) )
+console.log('p3', arr.reduce((a, v) => a + Math.abs(v-med), 0) )
