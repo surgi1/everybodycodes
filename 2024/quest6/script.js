5fbb67af986de207ba3aab141a5763db6c4bb87b
surgi <surgi1@gmail.com>
2024-11-12 08:05:41 +0100

    quest 6

---
+++ 2024/quest6/script.js
@@ -0,0 +1,33 @@
+const init = (input, nodes = {}) => {
+    input.split("\n").map(line => {
+        let tmp = line.split(/:|,/);
+        nodes[tmp[0]] = tmp.slice(1);
+    });
+    return nodes;
+}
+
+const run = (input, p1 = false) => {
+    const getPaths = (tree, curNode, curPath) => {
+        if (curPath.includes(curNode)) {
+            //console.log('cycle detected, stopping traverse', curNode, curPath);
+            return;
+        }
+        curPath.push(curNode);
+        if (tree[curNode] === undefined) return paths.push(curPath.slice());
+        tree[curNode].forEach(n => getPaths(tree, n, curPath.slice()))
+    }
+
+    let tree = init(input),
+        paths = [];
+
+    getPaths(tree, 'RR', []);
+    
+    let withApples = paths.filter(p => p[p.length-1] == '@');
+    let res = withApples.filter(p => withApples.filter(r => r.length == p.length).length === 1)
+
+    return p1 ? res[0].join('') : res[0].map(v => v[0]).join('');
+}
+
+console.log('p1', run(input1, true));
+console.log('p2', run(input2));
+console.log('p3', run(input3));
