d8d36c8d0a5b272cbf37ec85939a6e83062c2c21
surgi <surgi1@gmail.com>
2024-11-16 10:07:05 +0100

    q10

---
+++ 2024/quest10/input.js
@@ -0,0 +1,158 @@
+let inputt = `**XFZB**
+**LWQK**
+?G....WL
+BS....H?
+P?....KJ
+NM....Z?
+**NSHM**
+**PJGV**`;
+
+let input3 = `**X?DP**GKPZ**SKVD**GXPT**JLHS**PWZC**ZDNR**XJQW**ZNSR**VBTG**FKHW**GRPF**ZFDB**XSWL**CMBF**KXPJ**TQRH**VRCD**FVZX**GBZW**
+**VR??**HNWL**WZQJ**KCZF**CFZM**QBRS**FPKS**TBNM**MDWP**RXSN**PMJC**ZSNV**MJTQ**CPGH**NSTR**SWZN**FVXB**WNMG**STHQ**VPLC**
+RJ....MQ....?L....DV....KF....J?....W?....KZ....QB....?D....G?....FK....?X....MQ....PW....B?....W?....QF....CG....?F....WG
+DB....PS....?K....QX....T?....ZM....GP....N?....WC....N?....HT....?Q....VT....F?....NL....Z?....XG....?J....HD....V?....LK
+NF....VZ....?W....ZM....BC....?D....RX....?D....RX....?L....CV....J?....ZR....?H....VS....?C....PH....R?....WX....Z?....DB
+KG....TX....NH....?J....?G....HL....?B....SF....?J....PM....X?....WM....GN....?J....?G....TF....?K....TB....N?....QS....CP
+**MSNJ**SVMX**LHNM**QJVM**PDBG**LXNG**MBCW**VCSK**CXQF**FLPM**XDRQ**KWXM**CSXV**MBNJ**PZJW**RTFM**ZKNJ**QHXB**RNMJ**DTQK**
+**?FKG**BQTR**XGBR**BDRS**TRNK**DMHJ**GQXV**ZDRF**LTJB**ZHDC**VTGB**TQDJ**GRNH**VFDQ**XGLV**HCBG**GPWS**TFJZ**DWGC**SJFR**
+XZ....NB....JK....KC....Y?....Z?....BF....DY....NY....PY....TL....WQ....BS....QF....PN....FY....MC....QF....QV....WN....ZY
+FD....VK....?R....LR....DQ....KM....NJ....?Z....S?....G?....?D....?Y....LH....J?....KZ....?R....?B....WH....YS....BG....PN
+JL....SP....NY....Y?....FR....NY....?Z....WC....QW....SV....GY....BC....R?....BG....?T....LG....XY....L?....LZ....YL....BL
+QT....GM....VH....BP....TZ....QL....HY....KH....MX....XF....KZ....VJ....PY....YN....YH....JD....SV....VY....?T....K?....D?
+**DXTV**CZBN**QTRN**FKNC**JRSF**DBQV**FCKL**WXJH**VSPG**DJZM**XMPL**MJSC**FRCD**BHGM**GQWD**CBRZ**QKBJ**CTFN**WMLS**CLFR**
+**PQL?**JHKX**PSWF**WRDZ**ZMPX**NZXF**SNQJ**ZDVB**NTRL**GCXB**QNGT**VQNB**NQWS**VZNQ**SZKL**MVJT**HWPL**ZVLK**BFQJ**MPJD**
+ZW....TK....?Z....TV....Z?....RL....?N....HK....VQ....C?....?X....HN....ZC....?D....?V....WT....G?....HW....?V....HQ....RF
+FM....VD....CH....Q?....WC....?F....?G....JC....TX....K?....MZ....?P....?B....WQ....B?....DK....BC....D?....GC....?L....VJ
+NG....PX....BM....P?....?G....XS....V?....SL....?Z....LP....BD....?G....J?....NR....M?....SL....?V....RP....ZT....?W....CD
+LR....QS....NJ....R?....?M....PH....Q?....FP....BW....N?....?V....QL....?K....FT....GH....Q?....FZ....L?....?J....MK....NP
+**??FS**MSVW**ZBHV**MTPG**HKCD**SJRL**BPVH**QFPS**KZQC**RKLV**BJVZ**HZPT**MLTJ**RPDL**TMPV**QWKL**RZMD**QJRP**KCZG**VHKN**
+**KW?N**TPQD**XJMC**VQSX**GWNL**HMGP**GMDZ**CKLT**DWBX**WPNS**RDWH**KRGL**ZKVB**TWJF**JBHR**SFGD**CVFG**WDGH**VTNH**QWSB**
+NV....KL....GN....XM....D?....V?....?Y....ZR....?M....YD....Y?....F?....PX....FB....CJ....YN....RD....FT....W?....NZ....TS
+HC....WR....RB....HP....NB....CT....BX....G?....GY....VS....LX....CM....ZJ....?Y....DR....M?....JY....SY....HV....?M....XC
+TZ....FM....SW....VZ....KV....NY....QC....PX....HV....KP....TZ....RH....KY....HZ....?P....QR....KQ....KG....BY....YJ....WY
+GD....QS....CQ....JT....YQ....KQ....JH....KY....RF....F?....RP....YW....?C....SJ....FY....SW....T?....B?....LG....HS....?N
+**TVLC**LJMC**TNQP**ZMKQ**WQGX**CVWR**BWDG**FQMZ**TPGR**XBFH**PBCR**NHGS**LFRK**BLCK**NJTQ**GNQH**CZBF**FGPN**LSWD**MHQB**
+**DQH?**?XPY**RSWG**VDJL**ZVRS**KTLH**PHNS**HXVJ**DBXC**RQMC**ZGJM**JCMV**STPN**QMWG**DKLS**PSMV**WLKS**RVCB**TFVQ**TCKP**
+DS....QG....MF....?S....MH....G?....C?....DS....ZF....P?....MH....J?....XM....?F....?M....RS....GB....?T....?G....FL....CB
+NJ....LC....?X....ND....KT....X?....?P....QG....?V....DK....FQ....B?....NV....?Z....?X....DL....?P....CK....VH....S?....HV
+RV....FH....?Z....RB....Z?....DQ....HK....J?....LX....R?....CN....P?....SH....?D....BG....K?....HM....F?....?Q....XW....MP
+PB....TZ....?J....WG....L?....RN....L?....NW....MH....B?....?S....GR....CQ....R?....?W....VJ....ZN....S?....RN....?D....GQ
+**?B?R**YZRP**FKXM**HBWR**MTKD**XQDG**CTJK**LDSW**LWKZ**TPWG**HNQT**PWTR**MZVD**NDFX**GMRB**TDZB**TPHQ**TJSQ**HRBG**VDFG**
+**J?PG**?MNS**JZBD**NGST**NHJL**FNSP**LQVF**GNBC**MVFH**DNSK**SWXF**QBZX**CHXQ**ZTPR**VCWX**JKRL**NMJG**HWLK**XPNK**LWXS**
+KL....RB....ZH....NF....BL....KV....HY....XY....PX....GH....KQ....WV....SL....KY....DK....FX....RP....JG....BF....B?....KJ
+CS....NH....CV....MX....QW....DM....MV....W?....F?....DY....SR....PC....JX....ZD....JH....?L....JL....YF....JY....VM....LT
+FQ....PW....DS....QK....GS....HN....KR....VM....ZY....BJ....YG....?Y....?M....GM....SY....MB....MY....HX....QT....RN....WN
+TD....GJ....GW....BJ....JP....RT....?L....CT....JV....?L....X?....QM....DY....?S....B?....YN....S?....N?....?G....JY....Y?
+**LDK?**FHTG**CGHW**YXM?**PQWS**CTKG**VSDQ**MFCJ**RGZF**XDTP**WFSR**SDPL**XQWR**KWCX**VQDP**ZKPT**SDRB**TZQL**MBVZ**LKBV**
+**???C**?PYD**SVQN**HLWD**VBGR**ZJPM**FPMJ**SHZL**MQNP**ZCSB**VMXC**ZRJT**NFPJ**TGBP**NHKL**CRHM**FWNZ**RKHG**WDKP**XFNG**
+QF....KN....R?....QC....LR....V?....CT....Q?....?C....FG....CZ....?Q....DG....?F....CK....?Q....CK....?W....?F....DM....LG
+XS....CT....SB....?N....JC....?P....?J....DG....JZ....?S....DK....R?....?C....XV....?Z....HN....GM....N?....KQ....J?....KT
+HB....DJ....?J....VK....KW....N?....?Z....SN....HW....?R....TJ....?V....J?....WH....B?....LV....ZT....?L....CZ....V?....NC
+MV....LW....M?....HG....T?....GQ....K?....VP....L?....NV....XB....?F....TZ....N?....TP....D?....PF....X?....HT....?W....XF
+**XVBF**STPK**KDLM**QB?F**KLHJ**XQSL**ZCKN**XPVQ**SHXV**JVNK**QKTN**CFQW**DHZV**ZNQR**RTGS**FGLS**CMLX**NXSB**SHGF**CMPW**
+**NSMJ**GHY?**TBJR**KGYZ**MNCT**BNVH**GLTX**DGNW**CLWJ**QRFG**BZDJ**VMNG**LCGT**HFVJ**CJBZ**NVQD**GTPK**CWFD**QCTJ**JSDT**
+PC....VN....ZP....LM....WZ....TM....JT....XH....QD....P?....FJ....?S....RS....HB....V?....XZ....Y?....KD....DK....TM....YQ
+MT....SL....HF....BJ....PJ....DH....CQ....BK....?G....VY....?L....ZW....YJ....ZP....HM....NL....DG....YW....F?....YQ....S?
+JW....KB....DC....QT....CR....LQ....RW....ZN....BF....DG....PV....TL....?N....MF....KF....T?....KH....ST....NY....HV....MH
+DG....XF....KN....RS....XF....KN....SP....LG....YT....SK....YR....YN....DV....?Y....YC....YG....MR....H?....ZQ....?F....GF
+**KDCP**P?HT**ZPQH**PXBY**DWZX**HPFY**JHPS**VLTX**SKHP**TMRQ**RPFW**KGHC**HVJD**BDLN**WKVC**FRHX**JTLQ**MPQH**JCWG**PKNH**
+**?WGL**YJCG**FSNC**N?HQ**RPFQ**DC?V**BWRQ**GWMK**XDVF**GDZL**VDGQ**BSQX**STMB**PXVH**XDGQ**LBCD**WKFC**KVLR**ZTBV**BMLG**
+WJ....KZ....VW....?F....Q?....DM....NZ....H?....K?....RH....QX....?S....QN....?D....LC....W?....XC....?F....?V....DW....MP
+RV....GD....S?....LQ....?G....PR....RX....?D....ZG....P?....MT....?W....?X....TK....?N....FG....R?....QT....?B....NT....KX
+QM....CL....N?....KH....?K....ZW....?C....PV....?N....DW....ZJ....?V....C?....WS....H?....KV....?H....KW....S?....CZ....HB
+TB....PF....M?....CZ....LF....?S....F?....JS....?L....FS....LG....D?....GZ....?J....?P....DQ....LP....?J....R?....GJ....VL
+**VF?J**LDRC**XKBV**YWHV**NHMG**F?SD**DFMV**JNPD**GTLM**JXVW**SXZK**WVPZ**GQWZ**TWSJ**HZRB**KQGS**SPRD**DTJF**DMSN**VJWX**
+**M?B?**YVX?**LMWJ**?SNM**LSVK**CYWQ**ZCNX**HQZS**RZNW**HSFP**LTMJ**NRDF**NCXK**MRKC**PNLF**WZPV**XGBH**BSGW**HRPL**TZCD**
+SK....MQ....QN....KV....QB....LS....JP....MC....KB....RM....FW....SJ....ZR....DX....ZV....XR....FQ....HP....HW....GR....CX
+TJ....VP....JR....CL....MW....DH....BX....WD....LD....PW....QL....TH....ML....KQ....TW....LB....KB....JS....NV....DX....MY
+LZ....RX....XZ....DB....CJ....KN....NH....QV....ZJ....XN....DX....ZR....BP....CG....DC....HF....WC....DX....CK....SM....HV
+CW....FB....GT....WM....GZ....VP....GR....FZ....CV....GT....PC....KM....WT....FN....PK....MN....GL....RZ....TB....PL....?W
+**LXSW**TY?P**TZQD**RQL?**WCQD**JR?Q**JQGR**P?RY**DKBJ**TWYX**CPFH**NXZK**RDMF**HXDM**TDCV**NCY?**QJFK**MYTX**BVWG**?PJR**
+**CTPK**GMHQ**NCRG**JHBY**PZJB**BSYK**HBWP**XJLB**XPCV**NLG?**RWQD**SY?C**LTBP**KZ?Y**XWKM**MDHL**LCWZ**?ZPH**CTXK**DYVB**`;
+
+let inputt3 = `**XFZB**DCST**
+**LWQK**GQJH**
+?G....WL....DQ
+BS....H?....CN
+P?....KJ....TV
+NM....Z?....SG
+**NSHM**VKWZ**
+**PJGV**XFNL**
+WQ....?L....YS
+FX....DJ....HV
+?Y....WM....?J
+TJ....YK....LP
+**XRTK**BMSP**
+**DWZN**GCJV**`;
+
+let input2 = `**WZSX** **WXQB** **MWNF** **VMZQ** **LBVM** **FCHM** **PQZV** **XFNW** **TDFJ** **NFPD** **NJPC** **SMNC** **CHFM** **FVJB** **ZRTS**
+**DNRL** **STRD** **RJGB** **HKXR** **GDHC** **SWQJ** **MKFW** **DSBV** **QXGB** **CZRM** **FVTH** **VTLF** **STJP** **PKWL** **XNWM**
+ND....MP HZ....KV TF....XQ WQ....GH GB....ZT DB....ZN SF....PT SV....GQ CD....BS DL....JZ TK....JN TS....LG KV....LQ HJ....TQ PF....SK
+SG....XW TB....RW VP....BN DX....NS LS....HC VJ....QS QG....NX RX....WB FW....MH WN....MV QV....HS CJ....QN JS....RH BF....WK NQ....XD
+ZT....QR FL....QX WM....DK RL....KJ MR....DJ MF....HW DH....RW NF....DH GX....KT BP....FT GP....WR PF....MW WB....CM NM....RV LC....TR
+FL....JK NJ....DS JG....RZ BZ....MV QW....NV PC....KG VK....ZM PT....ML JQ....LP CR....GH CF....BD DX....HV FG....TP PS....LG ZM....BW
+**JFPQ** **VZHK** **VQDZ** **SWGN** **QSJW** **NDZG** **XSNT** **QRGL** **SPCK** **TGLB** **QBDG** **QJHG** **LBQK** **HTGQ** **LKDC**
+**TGKM** **FNJL** **XPTK** **LDJB** **RNTZ** **KVPB** **HRDG** **MPTH** **MWLH** **JVWH** **WRSK** **WDPX** **GVWR** **RSMN** **FBPQ**
+
+**CRQT** **GKWM** **BKHL** **GXSC** **RDCX** **TKPZ** **GJXT** **PDCT** **BVCF** **CZXV** **TGWS** **XQLZ** **HKTB** **NHSC** **WFMP**
+**SGXL** **BXFQ** **GVMD** **DRNM** **WGBH** **NWCM** **FKMH** **JHQB** **GWQX** **TPWH** **XNFD** **KSMG** **ZDPR** **VJXD** **DLTJ**
+NC....WQ NF....ZG QC....GT XG....NC JV....DF XM....KH LZ....CM NS....CV CW....GX NC....ZH ZC....LX ZN....LV BV....WC WS....KF QL....JK
+JD....HB HX....VM KP....LM QJ....SB CX....PL CZ....NW TN....FS KD....JM MN....FQ TX....QG RH....FK RJ....CQ PZ....NJ GZ....JT TZ....BV
+GL....KP WB....RQ SB....DH VW....RK MH....GW QJ....RF RJ....WB RH....GT HL....JD MV....SW QG....TS HX....SF KL....FM NC....LX FR....MD
+SX....RT JC....LK NW....VJ DM....PF RN....BT PV....DT GK....HX BQ....PZ BV....TZ BP....LD BN....WD MD....GK DH....RT HV....DR WX....PC
+**HNBW** **LNJZ** **WSTQ** **JFPQ** **PNMT** **QDXF** **WLSZ** **VZKN** **MLJZ** **LQDB** **BCQH** **JDHF** **LVCF** **FLRT** **BCQV**
+**PDKJ** **VRHC** **PCNJ** **WBVK** **FLVJ** **HRJV** **CNRB** **RSGM** **HNTD** **MSNG** **KRZL** **VNRC** **WJMN** **GKZW** **KZXR**
+
+**CLQH** **PVJZ** **FRQL** **KMPR** **HBRQ** **KWLZ** **RQFM** **KFNR** **RTNW** **GKFH** **NWJQ** **RSDT** **HZJP** **VNRL** **JBMZ**
+**KWGD** **RDLM** **SCBJ** **QBWX** **PMVG** **CBFN** **WZVH** **LZPS** **SQMC** **NTVC** **XTFL** **CWPQ** **GXDR** **GWPX** **VWQT**
+JH....VQ JP....ZV CQ....SM SM....JQ XC....LZ TW....HP JX....CV NS....LF QM....PV QB....GF KW....NV ST....ZC GR....QV PV....DQ CD....WK
+WK....GD HM....XG KR....LH BW....RC SJ....MK QB....LK SF....ZB JR....ZP HR....JD SH....XR JC....TP WP....MK WC....HP NT....LK RM....SB
+ZM....RS KQ....FT FV....ZJ DZ....HX WH....QR FN....GD HK....RD XD....MT CT....LK WV....KN QX....DG QN....HB ZS....BJ BJ....MW FN....VT
+CB....LN LR....DN XB....DG TP....FK PV....GB MZ....CJ MG....QW VW....QK WS....NX PT....CZ LF....SH LD....VR DL....KX GR....HX ZQ....JP
+**SMZR** **HXGN** **HGZM** **ZHSJ** **ZXCL** **TJMH** **BCGJ** **JWVQ** **KHLP** **PBXQ** **SDVK** **MVHK** **BQCK** **THKJ** **SPDR**
+**VJBN** **QKTF** **XVKD** **CFDT** **KWSJ** **GDPQ** **XDKS** **TDMX** **VXJD** **RSZW** **PHGC** **NBZL** **LWVS** **MDBQ** **KFNC**
+
+**LZMH** **CPMS** **BKRZ** **GWBL** **ZKFW** **GKFP** **SMNT** **NBTR** **SHXM** **CFHP** **KSZB** **VWTH** **RMXN** **WLQC** **ZNJD**
+**FXVN** **NJLK** **HJSC** **TFJR** **SDRV** **QBJM** **RVCW** **HFXQ** **BVWR** **XKST** **WCJH** **NBPS** **WFLG** **XBHF** **MQHR**
+CH....FS QN....MZ QR....MG HP....GJ JD....GS XP....BQ NW....VQ LQ....CB LQ....PD HK....CN RT....FH GM....JC CP....XH FB....XG PJ....QR
+MQ....GZ LR....HX LJ....XB BK....RW NR....CZ KC....GF LJ....SD KP....SJ WV....TS ZF....JR DV....PS LZ....HQ TS....VN LC....SH GS....ZB
+PK....BT WB....PS CH....SK NZ....DL FP....VQ DV....WS KR....BP FV....XN CH....KM VW....PS KX....ZN FV....BT BW....MG DM....QW LC....MV
+LN....VX DK....JC FZ....NW FT....VX KL....TW JR....TM HM....CT RG....TH GX....BR LX....QT JC....BW NW....SP LF....JR JP....TV XD....NH
+**QBCT** **WQDZ** **NQWL** **KHDP** **LPTN** **WRXC** **QKPB** **JGLP** **QGCT** **JWQZ** **TXDN** **CGLF** **HSTC** **STVP** **XVGL**
+**KSPG** **RHBX** **GFXM** **NZXV** **QCGJ** **TDSV** **HDJL** **CSKV** **KDPL** **VLRN** **VFRP** **ZQJM** **VPBJ** **JDGM** **PBCS**
+
+**FVZC** **MTVF** **RVWT** **CHKQ** **GNZX** **PQLJ** **ZNLG** **RBKC** **ZJSP** **JWMS** **PKXF** **SJLC** **HQJP** **SHMJ** **DQKP**
+**MDWN** **NBJQ** **DMFQ** **NZVX** **KRCH** **KFZS** **PRFX** **XGLN** **FCTW** **VPHG** **DJRM** **KPQH** **NMBK** **NFXV** **WXFG**
+VZ....HN NV....QB GR....TM QV....CR CG....PD MQ....LP KR....XP SR....GK ZD....RP JS....FH JT....XF CP....FL WM....ZD RX....DG CJ....FX
+XD....PJ DS....PJ LV....FN JM....ZD RX....TK SF....ZR BW....NL BN....JH CF....WL BT....QN BQ....HG KM....JQ LT....GK NM....JF TQ....NL
+FW....CL KF....TC ZP....QS KH....NX LM....NW TB....WG HF....ZQ VM....CW XQ....SG GW....RV MR....DN WS....HG NX....FJ HS....VP KB....GW
+GB....MQ WM....GL CB....WD BP....TL FZ....BH XJ....KC DC....VG LX....FP MH....TJ PL....MD SP....KV ZV....RX QH....PB WZ....LQ VD....PH
+**HLBG** **PLCD** **ZPGC** **PLJD** **FBWP** **BWGM** **VHCW** **VPJF** **QRDM** **NBTQ** **TQBS** **FGWM** **GFLX** **GDPR** **NHLJ**
+**PQXJ** **KSGW** **NBSL** **MRBT** **LDTM** **RCXT** **BDKQ** **HWMS** **HXLG** **LFRD** **HNVG** **VXZR** **WTZD** **QWZL** **CBVT**
+
+**HPFC** **ZJBQ** **CSNF** **RLVS** **XVKB** **XMCG** **SDRJ** **RLVH** **CTXG** **LKDV** **ZXTG** **ZCSM** **SKWC** **LQTM** **VCXN**
+**RQKL** **RVLP** **HQTZ** **DHZQ** **PQML** **TKJP** **PVFM** **DKJM** **KBQM** **BCPR** **QJVB** **KLXB** **DTMX** **ZKJN** **LGMS**
+VC....SP GJ....BQ GZ....CD CK....HZ PL....TK QJ....PM FT....DS PS....LB XG....FB PF....TR XQ....TF BH....NC XM....GK XB....KF XZ....WR
+MB....RK HN....RW HX....NS VJ....ST FS....DG TR....CZ GL....XM FR....HV PQ....NC XS....CW JV....BM JS....PK ZD....RJ LG....SJ KB....NV
+FQ....HG PD....XK TB....MQ QM....BR QM....ZB KG....FL PJ....VR WX....NZ TK....WR VK....GL CG....NZ RW....GT SV....TW MT....CP CQ....JH
+DJ....LT ZV....TL PL....RF XW....DL XN....RV SD....XH CZ....QN DK....MJ ZM....SD DJ....BZ LR....DK LZ....MX CQ....NL QW....NZ LM....SG
+**DJSG** **GNKH** **LPRX** **TJMC** **FTSD** **FHDR** **CXZT** **SNBZ** **ZNSP** **XZWJ** **MNCF** **RGWP** **NJZR** **XSWG** **QBKZ**
+**VMTB** **DXWT** **BDGM** **KBWX** **ZGNR** **QZLS** **GQLN** **XFWP** **FDWR** **FTGS** **LKRD** **NJHT** **GQLV** **CPBF** **RWJH**
+
+**BRLZ** **MBCD** **BJDR** **VCMP** **NCBX** **BQDX** **HTFR** **FGKR** **XPQL** **CWGX** **LCNZ** **PXGZ** **HZXV** **QBCL** **PJNM**
+**QHFJ** **TGSH** **XVMH** **RSZX** **LZMS** **ZJPN** **DPBL** **QMNW** **BSGK** **FKDS** **JQKM** **RWTB** **BQND** **DGMF** **FSCR**
+BF....HJ HT....VR RS....FX VD....FK LD....XZ WQ....BR NH....TS GQ....NL CJ....ST TQ....DL CZ....GN FS....QX HZ....NP KL....HD FZ....XL
+NV....CZ LQ....DB DB....CJ PS....MR WG....CV PC....ZN PV....BQ PC....FR BQ....MK CS....GK XK....PJ TC....WP XF....DQ XZ....GT GM....KH
+MK....RQ SG....NM KV....TM ZX....BC BS....NH JS....GD MD....JL XB....MT XL....PR PB....FZ MQ....RB HB....VJ BT....VM NS....CF SC....BV
+TG....DL JC....KZ HW....GL LH....JW JM....TP XL....KV RG....KF KD....SW GD....WZ WX....MH FS....VL ZR....NG WJ....LK QP....MB NJ....RP
+**GDMT** **KRQJ** **LSGC** **BLDK** **JTVW** **SVLG** **VGJS** **TCXL** **DWJZ** **HZMQ** **GSFV** **HNJC** **WTLJ** **ZKHP** **KLGV**
+**NVCK** **LZVN** **TWFK** **JFHW** **GHDP** **KCRW** **KMNQ** **SDPB** **TMRC** **TLBP** **RXBP** **FVSQ** **FKMP** **SNTX** **BHXZ**`;
+
+let input1 = `**VWHX**
+**CMRQ**
+TX....PR
+CQ....HW
+LK....JS
+MD....VN
+**KPSL**
+**TJDN**`;
---
+++ 2024/quest10/script.js
@@ -0,0 +1,83 @@
+const init = input => input.split("\n").map(line => line.split(''));
+const val = s => s.split('').map(c => c.charCodeAt(0) - 'A'.charCodeAt(0) + 1).reduce((a, v, i) => a+v*(i+1), 0);
+const extractMiddle = arr => arr.map(row => row.slice(2, 6)).slice(2, 6).flat();
+const isSolved = s => !s.includes('.');
+
+const solve = (arr, returnRaw = false) => {
+    arr.forEach((row, y) => row.forEach((v, x) => {
+        if (v != '.') return true;
+        let col = arr.map(r => r[x]);
+        let matches = row.filter(ch => ch != '.' && col.includes(ch));
+        if (matches.length == 1) {
+            arr[y][x] = matches[0];
+        }
+    }))
+
+    arr.forEach((row, y) => row.forEach((v, x) => {
+        if (v != '.') return true;
+        let col = arr.map(r => r[x]);
+        if (!(row.includes('?') || col.includes('?'))) return true;
+        let letters = {};
+        [...row, ...col].filter(l => !['?', '.'].includes(l)).forEach(l => {
+            letters[l] = letters[l] == undefined ? 1 : letters[l]+1;
+        })
+
+        let missingLetters = Object.entries(letters).filter(([l, count]) => count == 1).map(([l, count]) => l);
+
+        if (missingLetters.length !== 1) return true; // can't do
+
+        arr[y][x] = missingLetters[0];
+
+        // and fill questions marks
+        arr[y].forEach((l, pos) => (l == '?') && (arr[y][pos] = missingLetters[0]));
+        arr.forEach((r, fy) => (r[x] == '?') && (arr[fy][x] = missingLetters[0]));
+    }))
+
+    return returnRaw ? arr : extractMiddle(arr).join('');
+}
+
+const extractCard = (marr, col, row, spanX = 9, spanY = 9) => {
+    let arr = Array.from({length: 8}, () => []);
+    for (let y = 0; y < 8; y++) for (let x = 0; x < 8; x++) arr[y][x] = marr[row*spanY+y][col*spanX+x];
+    return arr;
+}
+
+const part2 = (input, cols = 15, rows = 7) => {
+    let marr = init(input), res = 0;
+
+    for (let y = 0; y < rows; y++) for (let x = 0; x < cols; x++) 
+        res += val(solve(extractCard(marr, x, y)));
+
+    return res;
+}
+
+const part3 = (input, cols = 2, rows = 2) => {
+    let marr = init(input),
+        res = 0, prevRes = 1;
+
+    const updateCard = (col, row, arr) => {
+        for (let y = 0; y < 8; y++) for (let x = 0; x < 8; x++) {
+            if (arr[y][x] != '?') marr[ row*6+y][ col*6+x] = arr[y][x];
+        }
+    }
+
+    while (prevRes != res) {
+        prevRes = res;
+        res = 0;
+
+        for (let y = 0; y < rows; y++) for (let x = 0; x < cols; x++) {
+            let card = extractCard(marr, x, y, 6, 6);
+            let solved = solve(card, true);
+            if (isSolved(solved.flat())) {
+                updateCard(x, y, solved);
+                res += val(extractMiddle(solved).join(''));
+            }
+        }
+    }
+
+    return res;
+}
+
+console.log('p1', solve(init(input1)));
+console.log('p2', part2(input2));
+console.log('p3', part3(input3, 20, 10));
