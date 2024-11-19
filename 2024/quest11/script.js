21b3a0a8250970d313bc8aba032b2d61f7cee580
surgi <surgi1@gmail.com>
2024-11-19 08:30:48 +0100

    q11

---
+++ 2024/quest11/script.js
@@ -0,0 +1,33 @@
+const init = (input, data = {}) => {
+    input.split("\n").map(line => {
+        let tmp = line.split(/:|,/g)
+        data[tmp[0]] = tmp.slice(1);
+    });
+    return data;
+}
+
+const solve = (data, starting = 'A', days = 4) => {
+    let population = {};
+    population[starting] = 1;
+    for (let i = 0; i < days; i++) {
+        let newPop = {};
+        Object.entries(population).forEach(([t, c]) => data[t].forEach(nt => newPop[nt] = newPop[nt] === undefined ? c : newPop[nt] + c ));
+        population = newPop;
+    }
+    return Object.values(population).reduce((a, v) => a+v, 0);
+}
+
+const part3 = (data, days = 20) => {
+    let min = Infinity; max = -Infinity;
+    Object.keys(data).forEach(k => {
+        let res = solve(data, k, days);
+        if (res < min) min = res;
+        if (res > max) max = res;
+    })
+
+    return max-min;
+}
+
+console.log('p1', solve(init(input1)));
+console.log('p2', solve(init(input2), 'Z', 10));
+console.log('p3', part3(init(input3)));
