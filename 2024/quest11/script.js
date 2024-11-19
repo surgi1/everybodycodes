be48ad6bc1c13971ba91ffbc9727ecf9f2111165
surgi <surgi1@gmail.com>
2024-11-19 10:32:20 +0100

    q11

--- 2024/quest11/script.js
+++ 2024/quest11/script.js
@@ -10,21 +10,16 @@ const solve = (data, starting = 'A', days = 4) => {
     let population = {};
     population[starting] = 1;
     for (let i = 0; i < days; i++) {
-        let newPop = {};
-        Object.entries(population).forEach(([t, c]) => data[t].forEach(nt => newPop[nt] = newPop[nt] === undefined ? c : newPop[nt] + c ));
-        population = newPop;
+        let next = {};
+        Object.entries(population).forEach(([t, c]) => data[t].forEach(n => next[n] = next[n] === undefined ? c : next[n] + c ));
+        population = next;
     }
     return Object.values(population).reduce((a, v) => a+v, 0);
 }
 
 const part3 = (data, days = 20) => {
-    let min = Infinity, max = -Infinity;
-    Object.keys(data).forEach(k => {
-        let res = solve(data, k, days);
-        min = Math.min(min, res);
-        max = Math.max(max, res);
-    })
-    return max-min;
+    let arr = Object.keys(data).map(k => solve(data, k, days));
+    return Math.max(...arr) - Math.min(...arr);
 }
 
 console.log('p1', solve(init(input1)));
