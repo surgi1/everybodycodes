c4effb014664d29c0ebab814580f63a437ad43fe
surgi <surgi1@gmail.com>
2024-11-21 11:03:00 +0100

    readability tweaks

--- 2024/quest12/script.js
+++ 2024/quest12/script.js
@@ -17,60 +17,51 @@ const vectR =  [1, 0];
 const vectDR = [1, 1];
 const vectDL = [-1, 1];
 
-const canHitTarget = (fromPos, _toPos, moving = false, shift = 0, maxPower = 50) => {
+const canHitTarget = (fromPos, _toPos, moving = 0, shift = 0, maxPower = 50) => {
     let toPos = [..._toPos];
     if (shift == 1) toPos = [0, 1].map(c => toPos[c] + vectDL[c]);
     for (let power = 1; power < maxPower; power++) {
-        if (!moving) {
-            // 3 parametric lines intersecting given coordinates
-            // 1st line: pos = fromPos + t*vectUp, t from 1 to power
-            // 2nd line: pos = (fromPos + power*vectUpR) + (t-power)*vectR, t from power to 2*power
-            // 3rd line: pos = (fromPos + power*vectUpR + power*vectR) + (t-2*power)*vectDownR, t from 2*power onwards
+        let from = [...fromPos], to = [...toPos], t = [0, 0];
+        // 3 parametric lines intersecting given coordinates
+        // 1st line: pos = from + t*vectUR, t from 1 to power
+        // 2nd line: pos = (from + power*vectUR) + (t-power)*vectR, t from power to 2*power
+        // 3rd line: pos = (from + power*vectUR + power*vectR) + (t-2*power)*vectDR, t from 2*power onwards
 
-            let t = [0, 1].map(c => (toPos[c] - fromPos[c])/vectUR[c]);
+        // as for moving targets, the 1st line would look like this: to + t*vectDL = from + t*vectUR => t = (to - from)/(vectUR-vectDL)
 
-            if (t[0] == t[1] && t[0] == Math.round(t[0]) && t[0] > 0 && t[0] <= power) return power;
+        // 1st line, going up
+        t = [0, 1].map(c => (to[c] - from[c])/(vectUR[c] - moving*vectDL[c]));
 
-            let newFromPos = [0, 1].map(c => fromPos[c] + power*vectUR[c]);
+        if (t[0] == t[1] && t[0] == Math.round(t[0]) && t[0] > 0 && t[0] <= power) return power;
 
-            if (newFromPos[1] == toPos[1]) {
-                t = [0].map(c => (toPos[c] - newFromPos[c])/vectR[c]);
+        from = [0, 1].map(c => from[c] + power*vectUR[c]);
+        to = [0, 1].map(c => to[c] + moving*power*vectDL[c]);
+
+        // 2nd line, horizontal
+        if (moving === 0) {
+            if (from[1] == to[1]) {
+                // only in this case they can possibly hit each other
+                t[0] = (to[0] - from[0])/vectR[0];
                 if (t[0] == Math.round(t[0]) && t[0] > 0 && t[0] <= power) return power;
             }
+        } else {
+            t = [0, 1].map(c => (to[c] - from[c])/(vectR[c] - vectDL[c]));
+            if (t[0] == t[1] && t[0] == Math.round(t[0]) && t[0] > 0 && t[0] <= power) return power;
+        }
 
-            newFromPos[0] += vectR[0]*power;
+        from = [0, 1].map(c => from[c] + power*vectR[c]);
+        to = [0, 1].map(c => to[c] + moving*power*vectDL[c]);
 
-            t = [0, 1].map(c => (toPos[c] - newFromPos[c])/vectDR[c]);
-
+        // 3rd line, falling down
+        if (moving === 0) {
+            t = [0, 1].map(c => (to[c] - from[c])/vectDR[c]);
             if (t[0] == t[1] && t[0] == Math.round(t[0])) return power;
         } else {
-            // 3 parametric lines intersecting given trajectory
-            // 1st line: pos = fromPos + t*vectUp, t from 1 to power
-            // 2nd line: pos = (fromPos + power*vectUpR) + (t-power)*vectR, t from power to 2*power
-            // 3rd line: pos = (fromPos + power*vectUpR + power*vectR) + (t-2*power)*vectDownR, t from 2*power onwards
-
-            // for 1st line, toPos + t*vectDL = fromPos + t*vectUR => t = (toPos - fromPos)/(vectUR-vectDL)
-
-            let t = [0, 1].map(c => (toPos[c] - fromPos[c])/(vectUR[c] - vectDL[c]));
-
-            if (t[0] == t[1] && t[0] == Math.round(t[0]) && t[0] > 0 && t[0] <= power) return power;
-
-            let newFromPos = [0, 1].map(c => fromPos[c] + power*vectUR[c]);
-            let newToPos = [0, 1].map(c => toPos[c] + power*vectDL[c]);
-
-            t = [0, 1].map(c => (newToPos[c] - newFromPos[c])/(vectR[c] - vectDL[c]));
-
-            if (t[0] == t[1] && t[0] == Math.round(t[0]) && t[0] > 0 && t[0] <= power) return power;
-
-            newFromPos = [0, 1].map(c => newFromPos[c] + power*vectR[c]);
-            newToPos = [0, 1].map(c => newToPos[c] + power*vectDL[c]);
-
-            if (newFromPos[1] == newToPos[1]) {
-                // only in this case they can hit each other
-                t = [0].map(c => (newToPos[c] - newFromPos[c])/(vectDR[c] - vectDL[c]));
-                if (t > 0 && t[0] == Math.round(t[0]) && (newToPos[1]+t[0] <= 2)) return power;
+            if (from[1] == to[1]) {
+                // only in this case they can possibly hit each other
+                t[0] = (to[0] - from[0])/(vectDR[0] - vectDL[0]);
+                if (t[0] > 0 && t[0] == Math.round(t[0]) && (to[1]+t[0] <= 2)) return power;
             }
-
         }
     }
     return false;
@@ -78,7 +69,7 @@ const canHitTarget = (fromPos, _toPos, moving = false, shift = 0, maxPower = 50)
 
 const rank = (catapult, power, target) => (catapult.val.charCodeAt(0) - 'A'.charCodeAt(0) + 1) * power * (target.val == 'H' ? 2 : 1);
 
-const solve = ([catapults, targets], movingTargets = false) => targets.reduce((sum, target, targetNr) => {
+const solve = ([catapults, targets], movingTargets = 0) => targets.reduce((sum, target, targetNr) => {
     let dist = Math.abs(catapults[2].pos[0]-target.pos[0]); // *1.5 as a good maximum comes from the projectile movement for lowest cannon; is spends 1/3 of its way going up and right, 1/3 just right, 1/3 going down. So in order to hit something as high as possible, power equal to 1.5*dist is needed
     if (movingTargets) dist = Math.abs(catapults[2].pos[1]-target.pos[1]); // here we need to take into account the movement and the ability to hit the rock on projectile's way up in worst case
     maxPower = Math.ceil(dist*1.5);
@@ -98,4 +89,4 @@ const solve = ([catapults, targets], movingTargets = false) => targets.reduce((s
 
 console.log('p1', solve(init(input1)));
 console.log('p2', solve(init(input2)));
-console.log('p3', solve(init(input3, targets3), true));
+console.log('p3', solve(init(input3, targets3), 1));
