bf8a326e8a17a8dc1f87a140a6b23ccbee52ef9e
surgi <surgi1@gmail.com>
2024-11-28 09:48:09 +0100

    minor tweaks

--- 2024/quest18/script.js
+++ 2024/quest18/script.js
@@ -8,53 +8,44 @@ const distanceMap = (map, froms) => {
         dist: 0
     }));
 
-    let filled = map.map(row => row.map(v => Infinity))
+    let dmap = map.map(row => row.map(v => Infinity)),
+        rows = map.length, cols = map[0].length;
 
     while (stack.length != 0) {
         let cur = stack.pop();
-        if (filled[cur.p[1]][cur.p[0]] <= cur.dist) continue;
-        filled[cur.p[1]][cur.p[0]] = cur.dist;
+        if (dmap[cur.p[1]][cur.p[0]] <= cur.dist) continue;
+        dmap[cur.p[1]][cur.p[0]] = cur.dist;
 
         DIRS.forEach(([dx, dy]) => {
-            let np = [cur.p[0]+dx, cur.p[1]+dy];
-            if (np[0] < 0 || np[0] > map[0].length-1 || np[1] < 0 || np[1] > map.length-1) return true;
-            if ('#' == map[np[1]][np[0]]) return true;
-            if (filled[np[1]][np[0]] < cur.dist) return true;
+            let p = [cur.p[0]+dx, cur.p[1]+dy];
+            if (p[0] < 0 || p[0] >= cols || p[1] < 0 || p[1] >= rows) return true;
+            if ('#' == map[p[1]][p[0]]) return true;
+            if (dmap[p[1]][p[0]] < cur.dist) return true;
             stack.push({
-                p: np,
+                p: p,
                 dist: cur.dist+1
             })
         })
     }
-    return filled;
+    return dmap;
 }
 
-const run1 = (map) => {
-    let dmap = distanceMap(map, [[0, map.map(row => row[0]).indexOf('.')]]);
-    let dists = [];
-    map.forEach((row, y) => row.forEach((v, x) => {
-        if (v == 'P') dists.push(dmap[y][x]);
-    }))
-    return Math.max(...dists);
-}
+const run = (map, p2 = false) => {
+    let dmap = distanceMap(map, p2 ? [[0, 1], [map[0].length-1, map.length-2]] : [[0, 1]]),
+        dist = 0;
 
-const run2 = (map) => {
-    let dmap = distanceMap(map, [[0, map.map(row => row[0]).indexOf('.')], [map[0].length-1, map.map(row => row[map[0].length-1]).indexOf('.')]]);
-    let dists = [];
     map.forEach((row, y) => row.forEach((v, x) => {
-        if (v == 'P') dists.push(dmap[y][x]);
+        if (v == 'P' && dmap[y][x] > dist) dist = dmap[y][x];
     }))
-    return Math.max(...dists);
+
+    return dist;
 }
 
 // takes ~5s
 const run3 = (map) => {
-    let min = Infinity;
-    let palms = [];
+    let min = Infinity, palms = [];
 
-    map.forEach((row, y) => row.forEach((v, x) => {
-        if (v == 'P') palms.push([x, y]);
-    }))
+    map.forEach((row, y) => row.forEach((v, x) => (v == 'P') && palms.push([x, y])))
 
     map.forEach((row, y) => row.forEach((v, x) => {
         if (v != '.') return true;
@@ -65,6 +56,6 @@ const run3 = (map) => {
     return min;
 }
 
-console.log('p1', run1(init(input1)));
-console.log('p2', run2(init(input2)));
+console.log('p1', run(init(input1)));
+console.log('p2', run(init(input2), true));
 console.log('p3', run3(init(input3)));
