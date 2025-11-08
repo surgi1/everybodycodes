60c7cd8d75834330806719776df6dd4670f4aae8
surgi <surgi1@gmail.com>
2025-11-08 20:16:20 +0100

    d1-d5

--- 2025/quest05/script.js
+++ 2025/quest05/script.js
@@ -1,54 +1,48 @@
 const parse = input => input.split(':').map(line => line.split(',').map(Number))
 
-const p1 = ([[id], data], returnRaw = false) => {
+const p1 = ([[id], data], p3 = false) => {
     let nodes = [];
 
-    const addNode = v => nodes.push({v: v})
-
     data.forEach(v => {
-        let foundNodeId = -1;
+        let placed = false;
         for (let nId = 0; nId < nodes.length; nId++) {
             let n = nodes[nId];
 
-            if (v < n.v && n.left === undefined) {
-                n.left = v;
-                foundNodeId = nId;
+            if (v < n[1] && n[0] === null) {
+                n[0] = v;
+                placed = true;
                 break;
             }
-            if (v > n.v && n.right === undefined) {
-                n.right = v;
-                foundNodeId = nId;
+            if (v > n[1] && n[2] === null) {
+                n[2] = v;
+                placed = true;
                 break;
             }
         }
 
-        if (foundNodeId < 0) addNode(v);
+        if (!placed) nodes.push([null, v, null])
     })
 
-    let q = Number(nodes.map(n => n.v).join(''));
+    let q = Number(nodes.map(n => n[1]).join(''));
 
-    if (!returnRaw) return q;
+    if (!p3) return q;
 
-    return {q: q, levels: nodes.map(n => (n.left !== undefined ? n.left : '') + '' + n.v + '' + (n.right !== undefined ? n.right : '') ).map(Number) };
+    return {q: q, levels: nodes.map(n => n.join('')).map(Number)};
 }
 
-const p2 = arr => {
-    let resArr = arr.map(([[id], data]) => p1([[id], data]));
-    return Math.max(...resArr) - Math.min(...resArr);
-}
+const p2 = arr => Math.max(...arr.map(v => p1(v))) - Math.min(...arr.map(v => p1(v)));
 
-const p3 = arr => {
-    let resArr = arr.map(([[id], data]) => ({id: id, ...p1([[id], data], true)})).sort((a, b) => {
-        if (a.q != b.q) return b.q - a.q;
+const p3 = arr => arr
+        .map(([[id], data]) => ({id: id, ...p1([[id], data], true)}))
+        .sort((a, b) => {
+            if (a.q != b.q) return b.q - a.q;
 
-        for (let i = 0; i < Math.min(a.levels.length, b.levels.length); i++)
-            if (a.levels[i] !== b.levels[i]) return b.levels[i] - a.levels[i];
+            for (let i = 0; i < Math.min(a.levels.length, b.levels.length); i++)
+                if (a.levels[i] !== b.levels[i]) return b.levels[i] - a.levels[i];
 
-        return b.id - a.id;
-    })
-
-    return resArr.reduce((a, v, i) => a + (i+1)*v.id, 0)
-}
+            return b.id - a.id;
+        })
+        .reduce((a, v, i) => a + (i+1)*v.id, 0)
 
 console.log('p1', p1(parse(input1)));
 console.log('p2', p2(input2.split('\n').map(parse)))
